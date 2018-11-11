3c3a0bf0d4306f3b7aba2c5fe1b0fab9e8eb8767
abhisheksaini <saini.abhishek@hotmail.com>
2018-11-11 20:00:26 +0100

    added sidebar html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {SidebarInterface} from './sidebar/interface/sidebar-interface';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,8 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
+
+  sidebarValue: SidebarInterface = {};
 
   ngOnInit(): void {
   }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -5,11 +5,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
+
 export class SidebarComponent implements OnInit {
+
+  type: string;
+  width: string;
+  state: string;
+  title: string;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  /***
+   * This function is to toggle the sidebar state and also to move up the scroll.
+   * @param {Event} event
+   * @param target
+   */
+  toggleState(event: Event, target: any) {
+    if (event.target['id'] === 'sidebar-wrapper' || event.target['id'] === 'close') {
+      this.state = 'inactive';
+      setTimeout(() => {
+        target.scrollIntoView();
+      }, 300);
+    }
+  }
+
 
 }
